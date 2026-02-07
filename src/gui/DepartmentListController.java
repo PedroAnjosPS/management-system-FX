1d8507d2ad8ee026fc164f539116c45faf0f3f65
PedroAnjosPS <phpsanches@gmail.com>
2026-02-07 03:01:43 -0300

    Remove department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,11 +1,13 @@
 package gui;
 
-import java.io.IOException; 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import application.Main;
+import db.DbIntegrityException;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
@@ -20,6 +22,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -45,6 +49,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
 	@FXML
 	private TableColumn<Department, Department> tableColumnEDIT;
+
+	@FXML
+	private TableColumn<Department, Department> tableColumnREMOVE;
 
 	@FXML
 	private Button btnNew;
@@ -83,8 +90,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
-		
+
 		initEditButtons();
+		initRemoveButtons();
 	}
 
 	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
@@ -119,13 +127,37 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
 	private void initEditButtons() {
 		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
-		
+
 		tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
 			private final Button button = new Button("edit");
 
 			@Override
 			protected void updateItem(Department obj, boolean empty) {
-				
+				super.updateItem(obj, empty);
+
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+
+				setGraphic(button);
+				setAlignment(Pos.CENTER);
+
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));
+			}
+		});
+
+	}
+
+	private void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		
+		tableColumnREMOVE.setCellFactory(param -> new TableCell<Department, Department>() {
+			private final Button button = new Button("remove");
+
+			@Override
+			protected void updateItem(Department obj, boolean empty) {
 				super.updateItem(obj, empty);
 				
 				if (obj == null) {
@@ -136,12 +168,33 @@ public class DepartmentListController implements Initializable, DataChangeListen
 				setGraphic(button);
 				setAlignment(Pos.CENTER);
 				
-				button.setOnAction(
-						event -> createDialogForm(obj, "/gui/DepartmentForm.fxml", 
-								Utils.currentStage(event)));
+				button.setOnAction(event -> removeEntity(obj));
 			}
 		});
 		
+	}
+
+	private void removeEntity(Department obj) {
+		Optional<ButtonType> result =  Alerts.showConfirmation("Confirmation", 
+			"Are you sure you want to delete the Department \"" 
+			 + obj.getName() 
+			 + "\" (Id: " 
+			 + obj.getId() + ")?");
+		
+		if (result.isPresent()
+			  && result.get().getButtonData() == ButtonBar.ButtonData.OK_DONE) {
+			if (service == null) {
+				throw new IllegalStateException("Service was null!");
+			}
+			
+			try {
+				service.remove(obj);
+				updateTableView();
+			}
+			catch (DbIntegrityException e) {
+				Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
 	}
 
 }
--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,7 +1,11 @@
 package gui.util;
 
+import java.util.Optional; 
+
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar;
 
 public class Alerts {
 	
@@ -11,5 +15,21 @@ public class Alerts {
 		alert.setHeaderText(header);
 		alert.setContentText(content);
 		alert.show();
+	}
+	
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		
+		ButtonType buttonOk = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+		ButtonType buttonCancel = new ButtonType(
+			"Cancel", ButtonBar.ButtonData.CANCEL_CLOSE
+		);
+		
+		alert.getButtonTypes().setAll(buttonOk, buttonCancel);
+		
+		return alert.showAndWait();
 	}
 }
